c72009f4c794a7d07d9446e482e861bb557eda8c
Project_Pannel <150807774+GudiyaPrasad@users.noreply.github.com>
2024-03-31 20:33:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from './store/reducer';
+
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = createStore(reducer);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+
+registerServiceWorker();
